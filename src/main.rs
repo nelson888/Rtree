f5da9079f0164dba149ecb236a1621ff3bab7301
nelson888 <tambue@hotmail.fr>
2019-06-27 08:03:06 +0200

    started implementing"|" printing on below lines

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::env;
 use std::io;
-use std::fs::{self, DirEntry};
+use std::fs::{self};
 use std::path::PathBuf;
 use std::ffi::OsStr;
-use std::fmt::Error;
 
 type Integer = i32;
 
@@ -28,23 +27,35 @@ fn print_path(path : &PathBuf) {
     }
 }
 
-//TODO print '|' in lines below
-fn visit(l : Integer, path: &PathBuf) -> io::Result<()> {
+//TODO bug in '|' printing in lines below
+fn visit(l : Integer, path: &PathBuf, branch_indexes : &mut Box<Vec<Integer>>) -> io::Result<()> {
     for _i in 0..l {
         print!("__");
     }
     print_path(path);
     print!("\n");
-    let nb_spaces: Integer = 2 * (l + l * 2 + (l * (l - 1)) / 2) - 2; //magic formula to print well
+    let nb_spaces: Integer = 2 * (l + l * 2 + (l * (l - 1)) / 2); //magic formula to print well
+    branch_indexes.push(nb_spaces);
+    let s_index : usize = branch_indexes.len() - 1;
     if path.is_dir() {
+        let files_count : usize = fs::read_dir(path)?.count();
+        let mut i : usize = 0;
         for r_entry in fs::read_dir(path)? {
-            for _i in 0..nb_spaces {
-                print!(" ");
+            for j in 0..nb_spaces {
+                if branch_indexes.contains(&j)  {
+                    print!("|");
+                } else {
+                    print!(" ");
+                }
             }
             print!("|");
+            if  i == files_count - 1 {
+                branch_indexes.remove(s_index);
+            }
+            i = i + 1;
             let entry = r_entry?;
             if path.is_dir() {
-                visit(l + 1, &entry.path())?;
+                visit(l + 1, &entry.path(), branch_indexes)?;
             } else {
                 print_path(path);
             }
@@ -55,6 +66,7 @@ fn visit(l : Integer, path: &PathBuf) -> io::Result<()> {
 
 fn main() -> std::io::Result<()> {
     let path = env::current_dir()?;
-    visit(0, &path)?;
+    let mut b:  Box<Vec<Integer>> =  Box::new(Vec::new());
+    visit(0, &path, &mut b)?;
     Ok(())
 }
